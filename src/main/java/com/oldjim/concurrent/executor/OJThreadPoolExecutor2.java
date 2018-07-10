7fb0d721da31d6bf76a83af33d00dc6804d41fea
jim lin <39576599+15724757448@users.noreply.github.com>
2018-07-10 20:09:48 +0800

    v2.0初步思路

--- src/main/java/com/oldjim/concurrent/executor/OJThreadPoolExecutor2.java
+++ src/main/java/com/oldjim/concurrent/executor/OJThreadPoolExecutor2.java
@@ -1,15 +1,18 @@
 package com.oldjim.concurrent.executor;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.AbstractExecutorService;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
+
 
 /**
  * 线程池 
@@ -41,7 +44,38 @@ import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
  * 
  * 下面我们通过分析面临的问题来思考各个问题的解决方案
  * 问题1的解决思路初步设想是将初始化线程的入口操作放到execute()中去，有任务进来在初始化线程
- * 问题2的
+ * 		所以现在我们需要一个变量来记录工作线程的数量，实现方法有两个
+ * 		第一个是使用workers.size()来获取工作线程数量，
+ * 			方案一需要依赖Set的size()实现，在非线程安全的Set类中，我们需要加锁来保证获取正确的数据。而在线程安全的Set类中，size()方法的实现往往非常复杂。
+ * 		第二个是增加一个volatile数字或原子数字workerCount来记录工作线程数量，
+ * 			方案二利用CAS保证线程数量的正确性，在需要临界区的时候加锁保证竞态条件下数据的正确性？
+ * 			思考：是否存在竞态条件？
+ * 			1：workerCount与workers是否有独占的原子操作？1.1workerCount与workers两者任一的改动是否是需要锁定另一个资源？1.2两者同时更改是否是原子性的？
+ * 				暂时的考虑是1.1：workerCount的更改不需要判断workers的状态，workers的更改也不需要workerCount的状态，比如说遍历workers的条件是workers.size>0而不是workerCount>0
+ * 						  1.2：workerCount和workers.size()允许有一段时间是不一致的。
+ * 			2:workerCount与runStatus是否有独占的原子操作？
+ * 				我认为有，workerCount的增加的条件必须为runStatus是运行状态的。所以这部分的操作需要加锁执行，考虑是否可优化呢（long高低位保证原子性）
+ * 		此版本暂时的解决思路是增加workerCount字段记录工作线程数量
+ * 问题2的解决思路是将使用与创建解耦，使用ThreadFactory来创建线程
+ * 问题3的解决思路是增加拒绝策略
+ * 问题4涉及到在运行期间修改参数，暂时不实现此功能
+ * 问题5是在用户任务中抛出异常时的处理逻辑，目前简单try-catch捕获并打印异常信息
+ * 问题6首先需要定义什么情况算是线程空闲，此版本定义的逻辑为X时间内从任务队列中获取不到任务，则线程空闲
+ * 		当线程有回收就意味着有线程的发放。
+ * 		目前线程的回收/发放想到的有三种实现
+ * 								回收											发放
+ * 			方案1（JDK实现）	    线程终止	   								重新创建线程
+ * 			方案2（优化方案1）	    线程阻塞              								    唤醒线程
+ * 			方案3（优化方案2）	短时间内阻塞线程，超时后线程终止				先唤醒阻塞，无阻塞线程则重新创建线程
+ * 		此版本使用方案1，简单实现
+ * 		
+ * 问题7我们有时需要线程池执行的总任务数，每个线程成功执行的任务数量，失败的任务数量等，此版本暂时不实现此功能
+ * 问题8首先需要定义什么样的线程为非核心线程，此版本定义非核心线程如下，开启条件：任务队列满开启非核心线程。关闭条件:线程为空闲线程
+ * 		初步方案为监听队列状态，队列满了则使用问题6中的方案一管理线程的收发
+ * 问题9在任务的开始和结束等可扩展的地方可以设置扩展点，扩展点暂不实现
+ * 问题10在上一个版本中，我们使用共享内存的方式来实现线程之间的调度，但是当线程阻塞时无法使用此方法来调度线程，所以此版本我们需要引入线程协作来实现阻塞状态下的线程调度
+ * 问题11是在使用无界队列时由于没有设置队列大小，导致内存飙高的一种解决思路---持久化任务队列 此版本我们暂不实现
+ * 
  * 
  * 
  */
@@ -73,9 +107,13 @@ public class OJThreadPoolExecutor2 extends AbstractExecutorService{
 	 */
 	private WriteLock rsWriteLock = rsLock.writeLock();
 	/**
-	 * 用于管理工作者，在shutdown时有并发风险，使用CopyOnWriteArraySet
+	 * 主锁，用于workers的锁定操作
 	 */
-	private Set<Worker> workers = new CopyOnWriteArraySet<Worker>();
+	private ReentrantLock mainLock = new ReentrantLock();
+	/**
+	 * 用于管理工作者，在shutdown时有并发风险，不能使用CopyOnWriteArraySet，因为CopyOnWriteArraySet遍历时使用的是快照，并不是最新数据
+	 */
+	private Set<Worker> workers = new HashSet<Worker>();
 	/**
 	 * 线程池的状态，用于实现线程的中断、终止
 	 */
@@ -107,7 +145,7 @@ public class OJThreadPoolExecutor2 extends AbstractExecutorService{
      * 启动所有线程
      */
     private void startAllWorker() {
-    	for (Worker w : workers) {
+		for (Worker w : workers) {
 			w.thread.start();
 		}
 	}
@@ -170,7 +208,12 @@ public class OJThreadPoolExecutor2 extends AbstractExecutorService{
 					task.run();
 				}else{
 					if(!isRunning(rs)){
-						workers.remove(this);
+						mainLock.lock();
+						try {
+							workers.remove(this);
+						} finally {
+							mainLock.unlock();
+						}
 						tryTerminate();
 						return;
 					}
